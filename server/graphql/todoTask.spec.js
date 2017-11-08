061ee274e466cdd468757fe240d41ed9cecb67a3
Finn Terdal <fterdal@gmail.com>
2017-11-08 15:07:52 -0600

    add some field tests

--- server/graphql/todoTask.spec.js
+++ server/graphql/todoTask.spec.js
@@ -10,11 +10,30 @@ const expect = chai.expect;
 
 describe('Todo Task', () => {
   // Tests
-  it('is a passing test', () => {
-    expect(false);
-  });
 
-  it('is a failing test', () => {
-    expect(false).to.equal(true);
-  });
+  describe('Fields', () => {
+
+    it('has a string field called id', () => {
+      expect(todoTaskType.getFields()).to.have.property('id');
+      expect(todoTaskType.getFields().id.type).to.deep.equals(graphql.GraphQLString);
+    });
+
+    it('has a string field called title', () => {
+      expect(todoTaskType.getFields()).to.have.property('title');
+      expect(todoTaskType.getFields().title.type).to.deep.equals(graphql.GraphQLString);
+    });
+
+    it('has a string field called text', () => {
+      expect(todoTaskType.getFields()).to.have.property('text');
+      expect(todoTaskType.getFields().text.type).to.deep.equals(graphql.GraphQLString);
+    });
+
+    it('has a boolean field called completed', () => {
+      expect(todoTaskType.getFields()).to.have.property('completed');
+      expect(todoTaskType.getFields().completed.type).to.deep.equals(graphql.GraphQLBoolean);
+    });
+
+  })
+
+
 });
