5e81a530c422ae4bef2690d2c675ca86d055824d
Finn Terdal <fterdal@gmail.com>
2017-11-08 12:08:49 -0600

    Simple test framework

---
+++ server/graphql/todoTask.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const graphql = require('graphql');
+const chai = require('chai');
+
+// const pokemon = require('./pokemon');
+
+// const pokemonType = pokemon.type;
+const expect = chai.expect;
+
+describe('Pokemon', () => {
+  // Tests
+  it('is a test', () => {});
+});
