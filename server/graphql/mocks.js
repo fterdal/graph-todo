536d75fd5dc144f3c733d72911c711bb43e0f045
Finn Terdal <fterdal@gmail.com>
2017-11-10 13:51:00 -0600

    refactors out some mock data into its own file and implements an AllTodoTasks resolver

---
+++ server/graphql/mocks.js
@@ -0,0 +1,22 @@
+const mockTodoTasks = [
+  {
+    id: 1,
+    title: 'groceries',
+    text: 'get milk, eggs, and bear traps',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'clothes shopping',
+    text: 'get some new pants and socks',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'letter to grandma',
+    text: "remember to mention grandpa's upcoming birthday",
+    completed: true,
+  },
+];
+
+module.exports = mockTodoTasks;
--- server/graphql/resolverMap.js
+++ server/graphql/resolverMap.js
@@ -1,11 +1,32 @@
+const mockTodoTasks = require('./mocks');
+
 const resolverMap = {
-  TodoTaskById: id => {
-    return {
-      id: '1',
-      title: 'groceries',
-      text: 'get milk, eggs, and bear traps',
-      completed: false,
-    }
+  TodoTaskById: ({ id }) => {
+    return mockTodoTasks.find(task => {
+      return task.id === id;
+    })
+  },
+  AllTodoTasks: () => {
+    return [
+      {
+        id: 1,
+        title: 'groceries',
+        text: 'get milk, eggs, and bear traps',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'clothes shopping',
+        text: 'get some new pants and socks',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'letter to grandma',
+        text: "remember to mention grandpa's upcoming birthday",
+        completed: true,
+      },
+    ]
   }
 };
 
