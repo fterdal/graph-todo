bcfd1c9c46ee6c2f963fcb126b6b04901c3570a7
Finn Terdal <fterdal@gmail.com>
2017-11-13 15:17:39 -0600

    there, that's a whole lot cleaner

--- server/graphql/resolverMap/userResolvers.spec.js
+++ server/graphql/resolverMap/userResolvers.spec.js
@@ -6,60 +6,20 @@ const chai = require('chai');
 const {
   resolverMap: { AllUsers, UserById },
 } = require('../index');
-const mockUsers = require('./mocks');
+const { mockUsers } = require('./mocks');
 
 const expect = chai.expect;
 
 describe('User Resolvers', () => {
 
   it('UserById takes an argument and returns a User', () => {
-    expect(UserById({ id: 1 })).to.deep.equal({
-      id: 1,
-      email: 'bobby@gmail.com',
-      todoLists: [
-        {
-          description: 'go do some stuff',
-          id: 1,
-          name: 'first list',
-          tasks: [
-            {
-              completed: false,
-              id: 1,
-              text: 'get milk, eggs, and bear traps',
-              title: 'groceries',
-            },
-            {
-              completed: false,
-              id: 2,
-              text: 'get some new pants and socks',
-              title: 'clothes shopping',
-            }
-          ]
-        },
-        {
-          description: 'time to feel productive!',
-          id: 5,
-          name: 'fifth list',
-          tasks: [
-            {
-              completed: true,
-              id: 3,
-              text: "remember to mention grandpa's upcoming birthday",
-              title: 'letter to grandma',
-            }
-          ]
-        }
-      ]
-    });
+    expect(UserById({ id: 1 })).to.deep.equal(mockUsers[0]);
   });
 
   it('AllUsers returns all Users', () => {
     const allUsers = AllUsers();
     expect(allUsers).to.have.length(4);
-    expect(allUsers[1]).to.deep.equal({
-      id: 2,
-      email: 'ellen@yahoo.com',
-    })
+    expect(allUsers[1]).to.deep.equal(mockUsers[1])
   });
 
 });
