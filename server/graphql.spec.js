6b7dca739085d7ed08e908cc38caa02369f9d683
Finn Terdal <fterdal@gmail.com>
2017-11-13 15:37:07 -0600

    skeleton of graphql query tests

---
+++ server/graphql.spec.js
@@ -0,0 +1,22 @@
+/* eslint new-cap:0 */
+'use strict';
+
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('GraphQL Queries', () => {
+
+  describe('TodoTask Queris', () => {
+
+  })
+
+  describe('User Queries', () => {
+
+  })
+
+  describe('TodoList Queries', () => {
+
+  })
+
+});
