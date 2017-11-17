68a4278d65ff00845c56e0601b4daf25da71032e
Finn Terdal <fterdal@gmail.com>
2017-11-17 14:35:08 -0600

    completely refacroting the mocks, starting to use methods to access and modify mock data (the proper way)

--- server/graphql/resolverMap/mocks.js
+++ server/graphql/resolverMap/mocks.js
@@ -1,104 +1,104 @@
-let mockTodoTasks = [
-  {
-    id: 1,
-    title: 'groceries',
-    text: 'get milk, eggs, and bear traps',
-    completed: false,
-  },
-  {
-    id: 2,
-    title: 'clothes shopping',
-    text: 'get some new pants and socks',
-    completed: false,
-  },
-  {
-    id: 3,
-    title: 'letter to grandma',
-    text: "remember to mention grandpa's upcoming birthday",
-    completed: true,
-  },
-  {
-    id: 4,
-    title: 'change oil in car',
-    completed: false,
-  },
-];
-const originalMockTodoTasks = {...mockTodoTasks};
-
-let mockTodoLists = [
-  {
-    id: 1,
-    name: 'first list',
-    description: 'go do some stuff',
-    tasks: [mockTodoTasks[0], mockTodoTasks[1]],
-  },
-  {
-    id: 2,
-    name: 'second list',
-    description: 'get busy!',
-    tasks: [mockTodoTasks[2], mockTodoTasks[3]],
-  },
-  {
-    id: 3,
-    name: 'third list',
-    description: "don't lay around, get out there!",
-    tasks: [mockTodoTasks[0], mockTodoTasks[3]],
-  },
-  {
-    id: 4,
-    name: 'fourth list',
-    description: 'time to feel productive!',
-    tasks: [mockTodoTasks[0], mockTodoTasks[1], mockTodoTasks[2]],
-  },
-  {
-    id: 5,
-    name: 'fifth list',
-    description: 'time to feel productive!',
-    tasks: [mockTodoTasks[2]],
-  },
-];
-const originalMockTodoLists = {...mockTodoLists};
-
-let mockUsers = [
-  {
-    id: 1,
-    email: 'bobby@gmail.com',
-    todoLists: [mockTodoLists[0], mockTodoLists[4]],
-  },
-  {
-    id: 2,
-    email: 'ellen@yahoo.com',
-    todoLists: [mockTodoLists[1]],
-  },
-  {
-    id: 3,
-    email: 'frank@hotmail.com',
-    todoLists: [mockTodoLists[2]],
-  },
-  {
-    id: 4,
-    email: 'janet@federalreserve.gov',
-    todoLists: [mockTodoLists[3]],
-  },
-];
-const originalMockUsers = Object.assign({}, mockUsers);
-
-const reset = () => {
-  console.log('originalMockUsers[0]', originalMockUsers[0]);
-  mockUsers = {...originalMockUsers};
-  mockTodoTasks = originalMockTodoTasks;
-  mockTodoLists = originalMockTodoLists;
-}
-
-console.log('mockUsers[0]', mockUsers[0]);
-mockUsers[0].email = 'bobbys new email dot com';
-console.log('mockUsers[0]', mockUsers[0]);
-reset();
-console.log('mockUsers[0]', mockUsers[0]);
+let mockTodoTasks = []
+let mockTodoLists = []
+let mockUsers = []
 
 module.exports = {
-  mockUsers,
-  mockTodoTasks,
-  mockTodoLists,
-  reset,
+
+  reset() {
+    mockTodoTasks = []
+    mockTodoLists = []
+    mockUsers = []
+  },
+
+  addTodoTask({title, text, completed = false}) {
+    const nextId = mockUsers.length + 1;
+    mockTodoTasks.push({nextId, title, text, completed});
+  },
+
+  listTodoTasks() {
+    return mockTodoTasks;
+  },
+  //
+  // mockTodoTasks : [
+  //   {
+  //     id: 1,
+  //     title: 'groceries',
+  //     text: 'get milk, eggs, and bear traps',
+  //     completed: false,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'clothes shopping',
+  //     text: 'get some new pants and socks',
+  //     completed: false,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'letter to grandma',
+  //     text: "remember to mention grandpa's upcoming birthday",
+  //     completed: true,
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'change oil in car',
+  //     completed: false,
+  //   },
+  // ],
+  //
+  // mockTodoLists : [
+  //   {
+  //     id: 1,
+  //     name: 'first list',
+  //     description: 'go do some stuff',
+  //     tasks: [this.mockTodoTasks[0], this.mockTodoTasks[1]],
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'second list',
+  //     description: 'get busy!',
+  //     tasks: [mockTodoTasks[2], mockTodoTasks[3]],
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'third list',
+  //     description: "don't lay around, get out there!",
+  //     tasks: [mockTodoTasks[0], mockTodoTasks[3]],
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'fourth list',
+  //     description: 'time to feel productive!',
+  //     tasks: [mockTodoTasks[0], mockTodoTasks[1], mockTodoTasks[2]],
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'fifth list',
+  //     description: 'time to feel productive!',
+  //     tasks: [mockTodoTasks[2]],
+  //   },
+  // ],
+  //
+  // mockUsers : [
+  //   {
+  //     id: 1,
+  //     email: 'bobby@gmail.com',
+  //     todoLists: [mockTodoLists[0], mockTodoLists[4]],
+  //   },
+  //   {
+  //     id: 2,
+  //     email: 'ellen@yahoo.com',
+  //     todoLists: [mockTodoLists[1]],
+  //   },
+  //   {
+  //     id: 3,
+  //     email: 'frank@hotmail.com',
+  //     todoLists: [mockTodoLists[2]],
+  //   },
+  //   {
+  //     id: 4,
+  //     email: 'janet@federalreserve.gov',
+  //     todoLists: [mockTodoLists[3]],
+  //   },
+  // ],
+
 };
