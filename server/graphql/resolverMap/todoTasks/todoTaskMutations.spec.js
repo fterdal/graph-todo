704c1babc34fb4a332ad201fb03baed92af95fe3
Finn Terdal <fterdal@gmail.com>
2017-11-18 00:45:50 -0600

    change mutation specs to use new mock methods

--- server/graphql/resolverMap/todoTasks/todoTaskMutations.spec.js
+++ server/graphql/resolverMap/todoTasks/todoTaskMutations.spec.js
@@ -6,16 +6,20 @@ const chai = require('chai');
 const {
   resolverMap: { completeTaskById }
 } = require('../../index');
-const { mockTodoTasks } = require('../mocks');
+const { todoTaskById } = require('../mocks');
 
 const expect = chai.expect;
 
 describe('TodoTask Mutation Resolvers', () => {
 
+    beforeEach(() => {
+      require('../mockSeed');
+    })
+
     it('completeTaskById takes an id argument and changes the matching todoTask', () => {
-      const todoTaskBefore = { ...mockTodoTasks[0] };
+      const todoTaskBefore = {...todoTaskById({id: 1})}
       const returnedTodoTask = completeTaskById({ id: 1 });
-      const todoTaskAfter = mockTodoTasks[0];
+      const todoTaskAfter = {...todoTaskById({id: 1})}
       expect(todoTaskBefore.completed).to.not.equal(todoTaskAfter.completed)
       expect(returnedTodoTask).to.deep.equal(todoTaskAfter);
     });
