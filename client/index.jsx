fd37f5b31418dab3a8cfb8b3964cb7ea2f47e5dc
Finn Terdal <fterdal@gmail.com>
2017-11-20 14:14:08 -0600

    hooks up an active Apollo client to React

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+  link: new HttpLink(),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <ApolloProvider>
+  <ApolloProvider client={client}>
     <h1>Hello from React & Apollo!</h1>
   </ApolloProvider>,
   document.getElementById('app')
