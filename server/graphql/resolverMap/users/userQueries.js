c3ad00447d6b14f02c39d1649d74f9960ba2df57
Finn Terdal <fterdal@gmail.com>
2017-11-20 11:00:55 -0600

    fixes user resolvers and specs

--- server/graphql/resolverMap/users/userQueries.js
+++ server/graphql/resolverMap/users/userQueries.js
@@ -1,14 +1,11 @@
-const { mockUsers } = require('../mocks');
+const {
+  user,
+  listUsers,
+} = require('../mocks');
 
 const userResolvers = {
-  userById: ({ id }) => {
-    return mockUsers.find(user => {
-      return user.id === id;
-    });
-  },
-  allUsers: () => {
-    return mockUsers
-  },
+  userById: ({ id }) => user({ id }),
+  allUsers: () => listUsers(),
 }
 
 module.exports = userResolvers;
--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/graphql', expressGraphql({
   schema,
-  rootValue: resolverMap,
+  rootValue : resolverMap,
   pretty : true,
   graphiql : true,
 }));
