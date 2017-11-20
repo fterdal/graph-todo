036cdee07cae176b2db626e99195565dfa371526
Finn Terdal <fterdal@gmail.com>
2017-11-20 13:31:53 -0600

    install apollo graphql-tag for making queries on the front-end

--- client/index.jsx
+++ client/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 
 ReactDOM.render(
-  <ApolloProvider store={store} client={client}>
+  <ApolloProvider>
     <h1>Hello from React & Apollo!</h1>
   </ApolloProvider>,
   document.getElementById('app')
