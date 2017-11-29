ee435231e6d3739a91bcaaaa1cd76eb08b075391
Finn Terdal <fterdal@gmail.com>
2017-11-29 17:21:15 -0600

    remove annoying deprecation warning about operators being aliased and hooks up postgres on app startup

--- server/index.js
+++ server/index.js
@@ -4,18 +4,17 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const expressGraphql = require('express-graphql');
 const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const passport = require('passport');
 const postgres = require('./postgres');
 
-console.log('postgres', postgres);
-
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sessionStore = new SequelizeStore({ db: postgres });
-console.log('\n~~~~~ HELLO ~~~~~~\n');
 const { schema, resolverMap } = require('./graphql');
 const PORT = process.env.PORT || 8080;
 const app = express();
-if (process.env.NODE_ENV !== 'production') require('../secrets');
+
+const notProduction = process.env.NODE_ENV !== 'production';
+if (notProduction) require('../secrets');
 
 // passport registration
 passport.serializeUser(({ id }, done) => done(null, id));
@@ -39,7 +38,7 @@ const createApp = () => {
     schema,
     rootValue : resolverMap,
     pretty : true,
-    graphiql : process.env.NODE_ENV !== 'production',
+    graphiql : notProduction,
   }));
 
   // static file-serving middleware
@@ -53,14 +52,14 @@ const createApp = () => {
 
 if (require.main === module) {
   sessionStore.sync()
-    .then(() => postgres.sync() )
+    .then(() => postgres.sync({ force: notProduction}) )
     .then(createApp)
     .then(() => {
       // Start Listening on specified port:
       app.listen(PORT, () => console.log(`Waiting for requests on port ${PORT}`));
     })
 } else {
-  // createApp()
+  createApp();
 }
 
 module.exports = app;
--- server/postgres/models/user.js
+++ server/postgres/models/user.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
 const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 const db = require('../db');
 
 const User = db.define('user', {
