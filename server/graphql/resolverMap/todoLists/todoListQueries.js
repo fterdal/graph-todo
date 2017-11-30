d6b2dcace7e3c2c7ab662d1721fa97a2413bb22a
Finn Terdal <fterdal@gmail.com>
2017-11-30 17:21:18 -0600

    Trying to get the TodoList tasks resolver to load that todoList's tasks from the database

--- server/graphql/resolverMap/todoLists/todoListQueries.js
+++ server/graphql/resolverMap/todoLists/todoListQueries.js
@@ -1,6 +1,36 @@
 const TodoList = require('../../../postgres/models/todoList');
+const TodoTask = require('../../../postgres/models/todoTask');
 
 module.exports = {
   todoListById: ({ id }) => TodoList.findById(id),
-  allTodoLists: () => TodoList.findAll(),
+  allTodoLists: async () => {
+    const results = await TodoList.findAll({
+      include: [{
+        model: TodoTask
+      }]
+    });
+    // console.log(results[0].todoTasks);
+    return results;
+  },
+  TodoList: {
+    tasks: () => {
+      console.log('~~~~~~~~~~~~~~~');
+      console.log('~~~~ HELLO ~~~~');
+      console.log('~~~~~~~~~~~~~~~');
+      return [
+        {
+          id: 3,
+          title: 'asd',
+          text: 'dgdfg',
+          comleted: true
+        },
+        {
+          id: 3,
+          title: 'asd',
+          text: 'dgdfg',
+          comleted: true
+        },
+      ]
+    }
+  }
 }
