480b09effb327fd84c18c0ff59184ab4d957f602
Finn Terdal <fterdal@gmail.com>
2017-12-01 09:56:14 -0600

    Okay, well, I might have a solution: use ES6 classes to define field resolvers for Object types like TodoList

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -2,31 +2,7 @@ const fs = require('fs');
 const { buildSchema } = require('graphql');
 
 const schemaTxt = fs.readFileSync('./server/graphql/schema.graphql', 'utf-8');
-let resolverMap = require('./resolverMap');
-let TodoListResolvers =  {
-  TodoList: {
-    name: () => { return 'a name' },
-    tasks: () => {
-      console.log('~~~~~~~~~~~~~~~');
-      console.log('~~~~ HELLO ~~~~');
-      console.log('~~~~~~~~~~~~~~~');
-      return [
-        {
-          id: 3,
-          title: 'asd',
-          text: 'dgdfg',
-          comleted: true
-        },
-        {
-          id: 4,
-          title: 'asdsdf',
-          text: 'dgdfgjghjghj',
-          comleted: false
-        },
-      ]
-    }
-  }
-}
+const resolverMap = require('./resolverMap');
 
 const schema = buildSchema(schemaTxt);
 // console.log('~~~~ SCHEMA ===>', schema);
--- server/graphql/resolverMap/todoLists/todoListQueries.js
+++ server/graphql/resolverMap/todoLists/todoListQueries.js
@@ -1,10 +1,29 @@
-const TodoList = require('../../../postgres/models/todoList');
+const TodoListModel = require('../../../postgres/models/todoList');
 const TodoTask = require('../../../postgres/models/todoTask');
 
+class TodoList {
+  constructor(id, name, description) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.tasks = [];
+    console.log('TODOLIST CLASS WAS CALLED!');
+  }
+
+  tasks() {
+    return this.tasks;
+  }
+}
+
 module.exports = {
-  todoListById: ({ id }) => TodoList.findById(id),
+  todoListById: async ({ id }) => await new TodoList(
+    5,
+    'fake list',
+    'fake description',
+  ),
+  // todoListById: ({ id }) => TodoListModel.findById(id),
   allTodoLists: async () => {
-    const results = await TodoList.findAll({
+    const results = await TodoListModel.findAll({
       include: [{
         model: TodoTask
       }]
@@ -12,26 +31,10 @@ module.exports = {
     // console.log(results[0].todoTasks);
     return results;
   },
-  TodoList: {
-    name: () => { return 'a name' },
-    tasks: () => {
-      console.log('~~~~~~~~~~~~~~~');
-      console.log('~~~~ HELLO ~~~~');
-      console.log('~~~~~~~~~~~~~~~');
-      return [
-        {
-          id: 3,
-          title: 'asd',
-          text: 'dgdfg',
-          comleted: true
-        },
-        {
-          id: 3,
-          title: 'asd',
-          text: 'dgdfg',
-          comleted: true
-        },
-      ]
-    }
-  }
+  // TodoList: () => ({
+  //   id: 1,
+  //   name: 'sdfsdf',
+  //   description: 'fghfghfgh',
+  //   tasks: null,
+  // })
 }
