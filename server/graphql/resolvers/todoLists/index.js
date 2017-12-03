b27f6391d93b8d2598c1f6bb07c2f281b4df4d4d
Finn Terdal <fterdal@gmail.com>
2017-12-03 13:30:02 -0600

    Deletes old unnecessary directories

--- server/graphql/resolvers/todoLists/index.js
+++
@@ -1,6 +0,0 @@
-const TodoList = require('../../../postgres/models/todoList');
-
-module.exports = {
-  allTodoLists: () => TodoList.findAll(),
-  todoListById: (_, { id }) => TodoList.findById(id),
-}
--- server/graphql/resolvers/todoTasks/index.js
+++
@@ -1,6 +0,0 @@
-const TodoTask = require('../../../postgres/models/todoTask');
-
-module.exports = {
-  allTodoTasks: () => TodoTask.findAll(),
-  todoTaskById: (_, { id }) => TodoTask.findById(id),
-}
