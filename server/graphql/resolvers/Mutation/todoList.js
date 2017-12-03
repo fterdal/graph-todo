0e5822589285048eb3e0ab5d71b5aff2236e6728
Finn Terdal <fterdal@gmail.com>
2017-12-03 15:39:11 -0600

    Cleaning up the mutation to create a new todoList with a user attached

--- server/graphql/resolvers/Mutation/todoList.js
+++ server/graphql/resolvers/Mutation/todoList.js
@@ -22,10 +22,21 @@ module.exports = {
         todoList.addTodoTask(todoTask);
       }));
     }
-    console.log('user', user);
-    console.log('req.user', req.user);
+    if (user) {
+      const foundUser = await User.findOne({ where: { email: user.email } });
+      if (!foundUser) throw new Error('User Not Found');
+      if (!foundUser.correctPassword(user.password)) throw new Error('Invalid Credentials');
+      if (req && req.user && req.user.id === foundUser.id) foundUser.addTodoList(todoList);
+    }
     // if (user && req.user) {
-    //
+    //   const foundUser = await User.find({
+    //     where: { email: user.email }
+    //   });
+    //   if (foundUser.id === req.user.id) {
+    //     foundUser.addTodoList(todoList);
+    //   } else {
+    //     throw new Error('Not Authorized');
+    //   }
     // }
     return todoList;
   },
