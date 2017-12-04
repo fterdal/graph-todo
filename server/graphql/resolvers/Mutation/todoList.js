611511d5579d32fe604a86de5ecfa94e315b6985
Finn Terdal <fterdal@gmail.com>
2017-12-04 14:15:14 -0600

    reformat createTodoList function parameter list (still not totally settled on it)

--- server/graphql/resolvers/Mutation/todoList.js
+++ server/graphql/resolvers/Mutation/todoList.js
@@ -1,35 +1,17 @@
 const { isSelfOrAdmin } = require('../utils');
 
 module.exports = {
+  // Not sure how to format this function. There's so much destructuring in the
+  // parameters list, and it should definitely be on more than one line.
   createTodoList: async (_, {
-    input: {
-      name,
-      description,
-      todoTasks,
-      userId,
-    } }, {
-      req,
-      res,
-      models: {
-        TodoTask,
-        TodoList,
-        User,
-      }
-    }
-  ) => {
+    input: { name, description, todoTasks, userId }
+  }, {
+    req, res, models: { TodoTask, TodoList, User }
+  }) => {
     isSelfOrAdmin(req, res, userId);
-    // TO-DO: Make sure that if the user's credentials are bad, nothing happens
-    // to the database (currently the todoList gets created, but the user is
-    // not associated with it)
     const user = await User.findById(userId);
-    // console.log('user', user);
     const todoList = await TodoList.create({ name, description });
     user.addTodoList(todoList);
-    // if (user) {
-    //   // const foundUser = await User.findOne({ where: { email: user.email } });
-    //   // if (!foundUser) throw new Error('User Not Found');
-    //   // if (!foundUser.correctPassword(user.password)) throw new Error('Invalid Credentials');
-    // }
     if (todoTasks && todoTasks.length) {
       const newTodoTasks = await Promise.all(todoTasks.map(todoTask => TodoTask.create(todoTask)));
       await Promise.all(newTodoTasks.map(todoTask => todoList.addTodoTask(todoTask)))
