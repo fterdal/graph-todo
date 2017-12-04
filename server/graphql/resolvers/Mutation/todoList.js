1a2973ea524f4c27089c07ae398bf595a9935fd7
Finn Terdal <fterdal@gmail.com>
2017-12-04 09:58:00 -0600

    Adds skeleton of utils with role enforcement

--- server/graphql/resolvers/Mutation/todoList.js
+++ server/graphql/resolvers/Mutation/todoList.js
@@ -1,10 +1,12 @@
+const { isSelfOrAdmin } = require('../utils');
+
 module.exports = {
   createTodoList: async (_, {
     input: {
       name,
       description,
       todoTasks,
-      user,
+      userId,
     } }, {
       req,
       models: {
@@ -12,23 +14,23 @@ module.exports = {
         TodoList,
         User,
       }
-    }) => {
-    try {
-      // TO-DO: Make sure that if the user's credentials are bad, nothing happens
-      // to the database (currently the todoList gets created, but the user is
-      // not associated with it)
-      const todoList = await TodoList.create({ name, description });
-      if (user) {
-        const foundUser = await User.findOne({ where: { email: user.email } });
-        if (!foundUser) throw new Error('User Not Found');
-        if (!foundUser.correctPassword(user.password)) throw new Error('Invalid Credentials');
-        if (req && req.user && req.user.id === foundUser.id) foundUser.addTodoList(todoList);
-      }
-      if (todoTasks && todoTasks.length) {
-        const newTodoTasks = await Promise.all(todoTasks.map(todoTask => TodoTask.create(todoTask)));
-        await Promise.all(newTodoTasks.map(todoTask => todoList.addTodoTask(todoTask)))
-      }
-      return todoList;
-    } catch(err) { console.error(err) }
+    }
+  ) => {
+
+    // TO-DO: Make sure that if the user's credentials are bad, nothing happens
+    // to the database (currently the todoList gets created, but the user is
+    // not associated with it)
+    const todoList = await TodoList.create({ name, description });
+    if (user) {
+      const foundUser = await User.findOne({ where: { email: user.email } });
+      if (!foundUser) throw new Error('User Not Found');
+      if (!foundUser.correctPassword(user.password)) throw new Error('Invalid Credentials');
+      if (req && req.user && req.user.id === foundUser.id) foundUser.addTodoList(todoList);
+    }
+    if (todoTasks && todoTasks.length) {
+      const newTodoTasks = await Promise.all(todoTasks.map(todoTask => TodoTask.create(todoTask)));
+      await Promise.all(newTodoTasks.map(todoTask => todoList.addTodoTask(todoTask)))
+    }
+    return todoList;
   },
 }
---
+++ server/graphql/resolvers/utils.js
@@ -0,0 +1,6 @@
+module.exports = {
+  isSelfOrAdmin: (req, userId) => {
+
+  },
+  isAdmin: () => {}
+}
