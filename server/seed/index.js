da5eef68882dc93ddd11eef76f53081fc7c2c475
Finn Terdal <fterdal@gmail.com>
2017-12-05 14:40:11 -0600

    This seed file is a mess. Gonna clean it up.

--- server/seed/index.js
+++ server/seed/index.js
@@ -1,9 +1,23 @@
-const { User } = require('../postgres/models');
+const { User, TodoList } = require('../postgres/models');
 
 const mockUsers = require('./mockUsers');
+const mockTodoLists = require('./mockTodoLists');
 
 const seed = async () => {
-  const users = await Promise.all(mockUsers.map(mockUser => User.create(mockUser)))
+  const users = await Promise.all(mockUsers.map(mockUser => {
+    return User.create(mockUser);
+  }));
+  const todoLists = Object.keys(mockTodoLists).map(async userEmail => {
+    const userToAssign = users.find(user => user.email === userEmail);
+    console.log('userEmail', userEmail);
+    const todoListsToAssign = Promise.all(mockTodoLists[userEmail].map(todoList => {
+      return userToAssign.addTodoList(todoList);
+      // return userToAssign.addTodoList({name: 'some name', description: 'some description'});
+    }))
+    console.log('todoListsToAssign', todoListsToAssign);
+    return Promise.all(todoListsToAssign);
+  })
+  return todoLists;
 }
 
 module.exports = seed;
