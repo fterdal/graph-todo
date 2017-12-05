1e85ee84240dc9b02c66997faad8aab4ae9fc60e
Finn Terdal <fterdal@gmail.com>
2017-12-05 16:37:03 -0600

    The seed file is pretty now, and there are no more unhandled promise errors! Yay!

--- server/seed/index.js
+++ server/seed/index.js
@@ -2,38 +2,31 @@ const { User, TodoList } = require('../postgres/models');
 
 const mockUsers = require('./mockUsers');
 const mockTodoLists = require('./mockTodoLists');
+const mockTodoTasks = {/* To Do: Make these */}
 
-const seedUsers = () => {
-  return Promise.all(mockUsers.map(mockUser => User.create(mockUser)))
-  // return users;
+const seedUsers = users => {
+  return Promise.all(users.map(mockUser => User.create(mockUser)))
 }
 
-const seedTodoLists = users => {
+const seedTodoLists = (users, todoLists) =>
+  Promise.all(
+    Object.entries(todoLists)
+    .map( ([ userEmail, lists ]) => {
+      const userToAssign = users.find(user => user.email === userEmail);
+      return Promise.all(lists.map(async list => {
+         userToAssign.addTodoLists( await TodoList.create(list) );
+      }))
+    })
+  )
+
+const seedTodoTasks = (todoLists, todoTasks) => {
 
 }
 
 const seed = async () => {
-  try {
-    const todoLists = Object.keys(mockTodoLists).map(userEmail => {
-      try {
-        const userToAssign = users.find(user => user.email === userEmail);
-        const todoListsToAssign = Promise.all(mockTodoLists[userEmail].map(async todoList => {
-          try {
-            return userToAssign.addTodoList( await TodoList.create(todoList));
-          } catch(err) {
-            console.error(err)
-          }
-        }))
-        return Promise.all(todoListsToAssign);
-      } catch(err) {
-        console.error(err)
-      }
-    })
-    console.log('~~~~~ ABOUT TO FINISH SEEDING ~~~~~');
-    return todoLists;
-  } catch(err) {
-    console.error(err)
-  }
+  const createdUsers = await seedUsers(mockUsers);
+  const createdTodoLists = await seedTodoLists(createdUsers, mockTodoLists);
+  return seedTodoTasks(createdTodoLists, mockTodoTasks)
 }
 
 module.exports = seed;
