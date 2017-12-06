811c9f1663cb1172fb0f352b53a28db07cf42220
Finn Terdal <fterdal@gmail.com>
2017-12-06 16:06:44 -0600

    cleanup

--- server/graphql/resolvers/Mutation/todoTask.js
+++ server/graphql/resolvers/Mutation/todoTask.js
@@ -1,18 +1,6 @@
-const { userIsLoggedIn, userCanEditTodoTask } = require('../utils');
+const { userCanEditTodoTask } = require('../utils');
 
 module.exports = {
-  // // I don't think I need this action at all. TodoTasks should always
-  // // be associated with a todoList. Creating one sby itself should not
-  // // be supported.
-  // createTodoTask: async (_, {
-  //   input: { title, text, completed }
-  // }, {
-  //   req, res, models: { TodoTask }
-  // }) => {
-  //   userIsLoggedIn(req, res);
-  //   const todoTask = await TodoTask.create({ title, text, completed });
-  //   return todoTask;
-  // },
   updateTodoTask: async (_,
   {
     input: { todoTaskId, input: { title, text, completed } }
