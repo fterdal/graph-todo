d3994ca6cae8f7b3e8b4720c3e1c5ce5e38dec5d
Finn Terdal <fterdal@gmail.com>
2017-12-15 13:07:17 -0600

    Creates a mutation that logs users out

--- server/graphql/resolvers/Mutation/auth.js
+++ server/graphql/resolvers/Mutation/auth.js
@@ -11,4 +11,13 @@ module.exports = {
     req.login(user, err => { if (err) console.error(err) });
     return user;
   },
+  logout: (_, __, { req } ) => {
+    const user = req.user;
+    req.logout();
+    // const user = await User.findOne({ where: { email } });
+    // if (!user) throw new Error('User Not Found');
+    // if (!user.correctPassword(password)) throw new Error('Invalid Credentials');
+    // req.login(user, err => { if (err) console.error(err) });
+    return user;
+  },
 }
