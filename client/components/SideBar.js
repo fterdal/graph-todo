f6c5be57e02b18d4ee77695e9dc536854887f09d
Finn Terdal <fterdal@gmail.com>
2017-12-15 11:56:03 -0600

    simplify sidebar props

--- client/components/SideBar.js
+++ client/components/SideBar.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
-const SideBar = (props) => {
-  const welcome = !props.data.me ? (<div>Login or Signup</div>)
-                                 : (<div>Welcome, {props.data.me.email}</div>);
+const SideBar = ({ data }) => {
+  const welcome = !data.me ? (<div>Login or Signup</div>)
+                           : (<div>Welcome, {data.me.email}</div>);
   return (
     <div className="sidebar-container">
       <div />
