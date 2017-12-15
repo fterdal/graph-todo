dc84957384478bd5272241bbd344d3d9e218c406
Finn Terdal <fterdal@gmail.com>
2017-12-15 12:05:05 -0600

    Improve sidebar layout

--- client/components/SideBar.js
+++ client/components/SideBar.js
@@ -3,16 +3,27 @@ import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const SideBar = ({ data }) => {
-  const welcome = !data.me ? (<div>Login or Signup</div>)
-                           : (<div>Welcome, {data.me.email}</div>);
-  return (
-    <div className="sidebar-container">
-      <div />
-      {welcome}
-      <a className="button auth-button">Login</a>
-      <a className="button auth-button">Signup</a>
-    </div>
-  )
+  const loggedIn = !!data.me;
+
+  if (loggedIn) {
+    return (
+      <div className="sidebar-container">
+        <div />
+        <div>Welcome, {data.me.email}</div>
+        <a className="button auth-button">Account</a>
+        <a className="button auth-button">Logout</a>
+      </div>
+    )
+  } else {
+    return (
+      <div className="sidebar-container">
+        <div />
+        <div />
+        <a className="button auth-button">Login</a>
+        <a className="button auth-button">Sign Up</a>
+      </div>
+    )
+  }
 }
 
 export const meQuery = gql`
