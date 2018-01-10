c2c68cdb5001e6ca48fa1796095522107b26f560
Finn Terdal <fterdal@gmail.com>
2018-01-10 09:28:49 -0600

    add coverage for Login component

--- client/components/auth/__tests__/Login.js
+++ client/components/auth/__tests__/Login.js
@@ -3,9 +3,19 @@ import renderer from 'react-test-renderer';
 import { Login } from '../Login';
 
 test('Login renders a form to log in', () => {
-  const sidebar = renderer.create(
+  const login = renderer.create(
     <Login />
   );
-  let sidebarTree = sidebar.toJSON();
-  expect(sidebarTree).toMatchSnapshot();
+  let loginTree = login.toJSON();
+  expect(loginTree).toMatchSnapshot();
+});
+
+test('User can enter in the login fields', () => {
+  const login = renderer.create(
+    <Login />
+  );
+  login.find('form').simulate('submit', { preventDefault: jest.fn() })
+  let loginTree = login.toJSON();
+  console.log('loginTree:', loginTree);
+  expect(loginTree).toMatchSnapshot();
 });
