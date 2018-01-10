02d759b237a8b3e874d213aa8833eaa1c948c64e
Finn Terdal <fterdal@gmail.com>
2018-01-09 18:18:17 -0600

    figuring out how jest coverage works

--- client/components/auth/__tests__/Login.js
+++ client/components/auth/__tests__/Login.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { StaticRouter } from 'react-router';
 import { Login } from '../Login';
 
 test('Login renders a form to log in', () => {
