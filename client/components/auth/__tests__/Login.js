774f321dc36f1c71353f2a3d9938f353c933b470
Finn Terdal <fterdal@gmail.com>
2018-01-10 11:45:08 -0600

    Adds enzyme and a sample test (for the Login component)

--- client/components/auth/__tests__/Login.js
+++ client/components/auth/__tests__/Login.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow, mount, render } from 'enzyme';
 import { Login } from '../Login';
 
 test('Login renders a form to log in', () => {
-  const login = renderer.create(
+  const login = shallow(
     <Login />
   );
-  let loginTree = login.toJSON();
-  expect(loginTree).toMatchSnapshot();
+  expect(login).toMatchSnapshot();
 });
 
 test('User can enter in the login fields', () => {
-  const login = renderer.create(
-    <Login />
+  const loginMutation = () => {}
+  const login = shallow(
+    <Login loginMutation={() => { console.log('~~~ login mutation was called! ~~~') }} />
   );
-  login.find('form').simulate('submit', { preventDefault: jest.fn() })
-  let loginTree = login.toJSON();
-  console.log('loginTree:', loginTree);
-  expect(loginTree).toMatchSnapshot();
+  login.find('#emailField').simulate('change', {
+    target: { value: 'hello@world.com' }
+  });
+  login.find('#passwordField').simulate('change', {
+    target: { value: 'secretPassword' }
+  });
+  expect(login.state().email).toBe('hello@world.com')
+  expect(login.state().password).toBe('secretPassword')
+  expect(login).toMatchSnapshot();
+  login.find('.auth-button').simulate('click')
+  expect(login).toMatchSnapshot();
 });
