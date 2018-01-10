af4687da54f6803438435dced810e00d6d2c7872
Finn Terdal <fterdal@gmail.com>
2018-01-10 11:58:28 -0600

    improve Login tests

--- client/components/auth/__tests__/Login.js
+++ client/components/auth/__tests__/Login.js
@@ -1,3 +1,4 @@
+/* eslint-env jest */
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import { Login } from '../Login';
@@ -10,9 +11,9 @@ test('Login renders a form to log in', () => {
 });
 
 test('User can enter in the login fields', () => {
-  const loginMutation = () => {}
+  const loginMutationMock = jest.fn();
   const login = shallow(
-    <Login loginMutation={() => { console.log('~~~ login mutation was called! ~~~') }} />
+    <Login loginMutation={loginMutationMock} />
   );
   login.find('#emailField').simulate('change', {
     target: { value: 'hello@world.com' }
@@ -23,6 +24,7 @@ test('User can enter in the login fields', () => {
   expect(login.state().email).toBe('hello@world.com')
   expect(login.state().password).toBe('secretPassword')
   expect(login).toMatchSnapshot();
-  login.find('.auth-button').simulate('click')
+  login.find('.auth-button').simulate('click');
+  expect(loginMutationMock).toHaveBeenCalled();
   expect(login).toMatchSnapshot();
 });
