34df1997af667eac1113efdfac443dc5da59c868
Finn Terdal <fterdal@gmail.com>
2018-01-10 17:02:02 -0600

    adds skeleton of test for auth

--- server/graphql/resolvers/Query/__tests__/auth.js
+++ server/graphql/resolvers/Query/__tests__/auth.js
@@ -1,5 +1,6 @@
 const auth = require('../auth');
 
 test('me resolves to null when user is not logged in', () => {
-  // TO DO
+  // TODO: Finish this up. Here's a skeleton
+  console.log('auth.me(null, null, {req: null})', auth.me(null, null, {req: { user: null } }));
 });
