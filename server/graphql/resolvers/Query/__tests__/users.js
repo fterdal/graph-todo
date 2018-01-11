fa2cfeca398e24ef7dff504625aa9ff4f8fa65be
Finn Terdal <fterdal@gmail.com>
2018-01-11 15:35:39 -0600

    adds tests for users (they look awfully similarly to those for todoTasks and todoLists)

---
+++ server/graphql/resolvers/Query/__tests__/users.js
@@ -0,0 +1,76 @@
+/* eslint-env jest */
+const users = require('../users');
+
+describe('allUsers', () => {
+
+  let req, res, models;
+  beforeEach(() => {
+    req = {
+      user: {
+        isAdmin: true,
+      }
+    };
+    res = { status: jest.fn() };
+    models = { User: { findAll: jest.fn() } };
+  })
+
+  test('resolves when user is admin', () => {
+    users.allUsers(null, null, { req, res, models });
+    expect(models.User.findAll).toHaveBeenCalled();
+  })
+
+  test('throws when user is not admin', () => {
+    req.user.isAdmin = false;
+    const userIsNotAdmin = () => {
+      users.allUsers(null, null, { req, res, models })
+    }
+    expect(userIsNotAdmin).toThrowError('Forbidden');
+    expect(res.status).toHaveBeenCalledWith(403);
+  })
+
+  test('throws when user is not logged in', () => {
+    req.user = null;
+    const userIsNotLoggedIn = () => {
+      users.allUsers(null, null, { req, res, models })
+    }
+    expect(userIsNotLoggedIn).toThrowError('Unauthorized');
+    expect(res.status).toHaveBeenCalledWith(401);
+  })
+});
+
+describe('userById', () => {
+
+  let req, res, models;
+  beforeEach(() => {
+    req = {
+      user: {
+        isAdmin: true,
+      }
+    };
+    res = { status: jest.fn() };
+    models = { User: { findById: jest.fn() } };
+  })
+
+  test('resolves when user is admin', () => {
+    users.userById(null, { id: 1 }, { req, res, models });
+    expect(models.User.findById).toHaveBeenCalled();
+  })
+
+  test('throws when user is not admin', () => {
+    req.user.isAdmin = false;
+    const userIsNotAdmin = () => {
+      users.userById(null, { id: 1 }, { req, res, models })
+    }
+    expect(userIsNotAdmin).toThrowError('Forbidden');
+    expect(res.status).toHaveBeenCalledWith(403);
+  })
+
+  test('throws when user is not logged in', () => {
+    req.user = null;
+    const userIsNotLoggedIn = () => {
+      users.userById(null, { id: 1 }, { req, res, models })
+    }
+    expect(userIsNotLoggedIn).toThrowError('Unauthorized');
+    expect(res.status).toHaveBeenCalledWith(401);
+  })
+});
