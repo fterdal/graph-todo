6b71705a69067d62fecd3a6f7e87498503219902
Finn Terdal <fterdal@gmail.com>
2018-01-11 15:30:25 -0600

    write a test for todoTaskById

--- server/graphql/resolvers/Query/__tests__/todoTasks.js
+++ server/graphql/resolvers/Query/__tests__/todoTasks.js
@@ -38,6 +38,39 @@ describe('allTodoTasks', () => {
   })
 });
 
-test('me resolves to user data when user is logged in', () => {
+describe('todoTaskById', () => {
 
+  let req, res, models;
+  beforeEach(() => {
+    req = {
+      user: {
+        isAdmin: true,
+      }
+    };
+    res = { status: jest.fn() };
+    models = { TodoTask: { findById: jest.fn() } };
+  })
+
+  test('resolves when user is admin', () => {
+    todoTasks.todoTaskById(null, { id: 1 }, { req, res, models });
+    expect(models.TodoTask.findById).toHaveBeenCalled();
+  })
+
+  test('throws when user is not admin', () => {
+    req.user.isAdmin = false;
+    const userIsNotAdmin = () => {
+      todoTasks.todoTaskById(null, { id: 1 }, { req, res, models })
+    }
+    expect(userIsNotAdmin).toThrowError('Forbidden');
+    expect(res.status).toHaveBeenCalledWith(403);
+  })
+
+  test('throws when user is not logged in', () => {
+    req.user = null;
+    const userIsNotLoggedIn = () => {
+      todoTasks.todoTaskById(null, { id: 1 }, { req, res, models })
+    }
+    expect(userIsNotLoggedIn).toThrowError('Unauthorized');
+    expect(res.status).toHaveBeenCalledWith(401);
+  })
 });
