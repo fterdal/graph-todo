4efab08ddd360777fb74b17b7bc27653da2200b1
Finn Terdal <fterdal@gmail.com>
2018-01-11 14:17:22 -0600

    adds a test for auth me

--- server/graphql/resolvers/Query/__tests__/auth.js
+++ server/graphql/resolvers/Query/__tests__/auth.js
@@ -1,6 +1,20 @@
+/* eslint-env jest */
 const auth = require('../auth');
 
 test('me resolves to null when user is not logged in', () => {
-  // TODO: Finish this up. Here's a skeleton
-  console.log('auth.me(null, null, {req: null})', auth.me(null, null, {req: { user: null } }));
+  expect(auth.me(null, null, {req: { user: null } })).toBe(null);
+});
+
+test('me resolves to user data when user is logged in', () => {
+  const user = {
+    id: 12,
+    email: 'hello@world.com',
+    todoLists: [
+      {
+        id: 7,
+        title: 'my list',
+      }
+    ]
+  }
+  expect(auth.me(null, null, {req: { user } })).toBe(user);
 });
