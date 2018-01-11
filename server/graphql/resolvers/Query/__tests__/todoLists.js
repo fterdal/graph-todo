aae4044a0a368c06936b7ebaa777d0f430708c7e
Finn Terdal <fterdal@gmail.com>
2018-01-11 14:59:17 -0600

    refactors the todoLists tests to be more organized

--- server/graphql/resolvers/Query/__tests__/todoLists.js
+++ server/graphql/resolvers/Query/__tests__/todoLists.js
@@ -1,45 +1,39 @@
 /* eslint-env jest */
 const todoLists = require('../todoLists');
 
-test('allTodoLists retrieves all todolists', () => {
-  const req = {
-    user: {
-      isAdmin: true,
+describe('allTodoLists', () => {
+
+  let req, res, models;
+  beforeEach(() => {
+    req = {
+      user: {
+        isAdmin: true,
+      }
+    };
+    res = { status: jest.fn() };
+    models = { TodoList: { findAll: jest.fn() } };
+  })
+
+  test('resolves when user is admin', () => {
+    todoLists.allTodoLists(null, null, { req, res, models });
+    expect(models.TodoList.findAll).toHaveBeenCalled();
+  })
+
+  test('throws when user is not admin', () => {
+    req.user.isAdmin = false;
+    const userIsNotAdmin = () => {
+      todoLists.allTodoLists(null, null, { req, res, models })
     }
-  };
-  const res = { status: jest.fn() };
-  const models = { TodoList: { findAll: jest.fn() } };
+    expect(userIsNotAdmin).toThrowError('Forbidden');
+    expect(res.status).toHaveBeenCalledWith(403);
+  })
 
-  // User is admin
-  todoLists.allTodoLists(null, null, { req, res, models });
-  expect(models.TodoList.findAll).toHaveBeenCalled();
-
-  // User is not admin
-  req.user.isAdmin = false;
-  const userIsNotAdmin = () => {
-    todoLists.allTodoLists(null, null, { req, res, models })
-  }
-  expect(userIsNotAdmin).toThrowError('Forbidden');
-  expect(res.status).toHaveBeenCalledWith(403);
-
-  // User is not logged in
-  req.user = null;
-  const userIsNotLoggedIn = () => {
-    todoLists.allTodoLists(null, null, { req, res, models })
-  }
-  expect(userIsNotAdmin).toThrowError('Unauthorized');
+  test('throws when user is not logged in', () => {
+    req.user = null;
+    const userIsNotLoggedIn = () => {
+      todoLists.allTodoLists(null, null, { req, res, models })
+    }
+    expect(userIsNotLoggedIn).toThrowError('Unauthorized');
+    expect(res.status).toHaveBeenCalledWith(401);
+  })
 });
-
-// test('me resolves to user data when user is logged in', () => {
-//   const user = {
-//     id: 12,
-//     email: 'hello@world.com',
-//     todoLists: [
-//       {
-//         id: 7,
-//         title: 'my list',
-//       }
-//     ]
-//   }
-//   expect(todoLists.me(null, null, {req: { user } })).toBe(user);
-// });
