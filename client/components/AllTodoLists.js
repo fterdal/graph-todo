bb56a5c92fa806b8315116156442b21e4c8f89c5
Finn Terdal <fterdal@gmail.com>
2018-01-16 20:44:40 -0600

    Adds some simple css styling to the todoLists based on whether their todoTasks are done or not

--- client/components/AllTodoLists.js
+++ client/components/AllTodoLists.js
@@ -6,7 +6,19 @@ export const AllTodoLists = ({ data }) => {
   console.log('AllTodoLists data', data);
   if (!data.me || !data.me.todoLists) return (<div>Loading...</div>)
   const todoLists = data.me.todoLists.map(list => {
-    return (<li key={list.name + list.id}>{list.name}</li>)
+    const numTasks = list.todoTasks.length;
+    const numCompleted = list.todoTasks.filter(task => task.completed).length;
+    let allDone;
+    if (numTasks && numTasks !== numCompleted) allDone = true;
+    return (
+    <li
+      key={list.name + list.id}
+      style={{
+        fontWeight: allDone ? 'bold' : 'normal',
+        textDecoration: allDone ? 'normal' : 'line-through',
+      }}>
+      {list.name} <span>({numCompleted} / {numTasks})</span>
+    </li>)
   })
   return (
     <div className="container">
@@ -25,6 +37,10 @@ export const myTodoListsQuery = gql`
       todoLists {
         id
         name
+        todoTasks {
+          id
+          completed
+        }
       }
     }
   }
