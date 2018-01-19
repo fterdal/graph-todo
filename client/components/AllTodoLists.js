7a8138a0caecce1d3965f763ccaaefc2d251c4b0
Finn Terdal <fterdal@gmail.com>
2018-01-19 15:34:04 -0600

    reformat code for AllTodoLists component

--- client/components/AllTodoLists.js
+++ client/components/AllTodoLists.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { graphql, compose } from 'react-apollo';
 import { Link } from 'react-router-dom';
 import gql from 'graphql-tag';
-import { SingleTodoLists } from './index';
 
-export const AllTodoLists = ({ data, match }) => {
-  // console.log('match', match);
-  // console.log('AllTodoLists data', data);
+export const AllTodoLists = ({ data }) => {
   if (!data.me || !data.me.todoLists) return (<div>Loading...</div>)
   const todoLists = data.me.todoLists.map(list => {
     const numTasks = list.todoTasks.length;
@@ -14,17 +11,18 @@ export const AllTodoLists = ({ data, match }) => {
     let allDone;
     if (numTasks && numTasks !== numCompleted) allDone = true;
     return (
-    <li
-      key={list.name + list.id}
-      style={{
-        fontWeight: allDone ? 'bold' : 'normal',
-        textDecoration: allDone ? 'normal' : 'line-through',
-      }}>
-      <Link to="/lists/1">
-        {list.name} <span>({numCompleted} / {numTasks})</span>
-      </Link>
-    </li>)
+      <li
+        key={list.name + list.id}
+        style={{
+          fontWeight: allDone ? 'bold' : 'normal',
+        }}>
+        <Link to={`/lists/${list.id}`}>
+          {list.name} <span>({numCompleted} / {numTasks})</span>
+        </Link>
+      </li>
+    )
   })
+
   return (
     <div className="container">
       <h1>Your Todo Lists</h1>
