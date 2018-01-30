1cf21ed7daf736256b6bb9d75e8985d65f0e3fc3
Finn Terdal <fterdal@gmail.com>
2018-01-30 16:08:11 -0600

    It kinda works to click and complete todotasks. On to loads of refactoring now so its not terribly ugly

--- client/components/SingleTodoTask.js
+++ client/components/SingleTodoTask.js
@@ -1,39 +1,18 @@
 import React, { Component } from 'react';
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
+import { myTodoListQuery } from './SingleTodoList';
 
-// export const SingleTodoTask = ({ data }) => {
-export class SingleTodoTask extends Component {
-
-  state = {
-
-  }
-
-  render() {
-    console.log('this.props', this.props);
-    const data = this.props.data;
-
-    if (!data.todoTaskById) return (<div>Loading...</div>)
-    const { todoTaskById: { title, completed } } = data;
-    const style = {
-      textDecoration: completed ? 'line-through' : 'none',
-    };
-    return (
-      <li style={style}>{title}</li>
-    )
-  }
-}
-
+/***** GraphQL *****/
 export const myTodoTaskQuery = gql`
   query myTodoTask($id: Int!){
-  	todoTaskById(id: $id) {
+    todoTaskById(id: $id) {
       id
       title
       completed
     }
   }
 `;
-
 export const toggleCompleteTodoTaskMutation = gql`
   mutation toggleCompleteTodoTask($id: Int!, $completed: Boolean!) {
     updateTodoTask(input: {
@@ -43,19 +22,48 @@ export const toggleCompleteTodoTaskMutation = gql`
       }
     }) {
       id
-      title
-      text
-      completed
     }
   }
 `;
+
+/***** React Component *****/
+export class SingleTodoTask extends Component {
+  _toggleComplete = async () => {
+    if (!this.props.data.todoTaskById) return; // Data hasn't been loaded yet
+    let { todoTaskById: { id, completed } } = this.props.data;
+    completed = !completed;
+    await this.props.toggleCompleteTodoTaskMutation({
+      variables: {
+        id,
+        completed,
+      },
+      refetchQueries: [
+        {
+          query: myTodoTaskQuery,
+          variables: { id, completed },
+        },
+      ],
+    })
+  }
+  render() {
+    const data = this.props.data;
+    if (!data.todoTaskById) return (<div>Loading...</div>)
+    const { todoTaskById: { title, completed } } = data;
+    const style = {
+      textDecoration: completed ? 'line-through' : 'none',
+    };
+    return (
+      <li style={style} onClick={this._toggleComplete}>{title}</li>
+    )
+  }
+}
 
 export default compose(
   graphql(myTodoTaskQuery, {
     options: ({ id }) => ({ variables: { id } }),
   }),
   graphql(toggleCompleteTodoTaskMutation, {
-    options: ({ id }) => ({ variables: { id } }),
+    // options: ({ id }) => ({ variables: { todoTaskId: id, input:  } }),
     props: ({ mutate }) => ({ toggleCompleteTodoTaskMutation: mutate }),
   }),
 )(SingleTodoTask)
